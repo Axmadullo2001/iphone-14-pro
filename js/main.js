da1313e0b2d3aa12a1084879000afba55f0673be
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-11-02 19:24:44 +0300

    IPhone 14

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ const menu_btn = document.querySelector(".menu-btn")
 
 menu_btn?.addEventListener("click", () => {
     nav?.classList.toggle("menu-open")
+    if (nav?.classList.contains("menu-open")) {
+        document.body.style.overflow = "hidden"
+    }else {
+        document.body.style.overflow = "auto"
+    }
 })
+
